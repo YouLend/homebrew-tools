4feb89869f332455c16a6106690ed5534947ebbb
nikomain <nikolai.main@youlend.com>
2025-05-07 12:43:40 +0100

    Uploading brew formula to yl-homebrew-tools

---
+++ Formula/th.rb
@@ -0,0 +1,27 @@
+class Th < Formula
+  desc ""
+  homepage "https://github.com/nikomain/brew-scripts"
+  url "https://github.com/YouLend/homebrew-scripts/archive/refs/tags/th-v1.1.0.tar.gz"
+  sha256 "e71a18fe905e78b2ea1f088da12a00918f002e1916885052536685dc1e55712c"
+  license ""
+
+  def install
+    Dir.chdir("brew-scripts/th") do
+      bin.install "installer.sh" => "th-install"
+      pkgshare.install "th-functions.sh"
+    end
+  end
+
+  def caveats
+    <<~EOS
+      To complete installation, run:
+
+        th-install
+
+      This will:
+        - Install Teleport (if missing)
+        - Configure your shell (e.g., .bashrc or .zshrc)
+
+    EOS
+  end
+end
