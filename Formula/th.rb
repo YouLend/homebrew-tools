7f237d1b6b24174e55acc899c70841eaf51043b3
nikomain <nikolai.main@youlend.com>
2025-07-20 14:29:23 +0000

    Update th formula to 1.3.4

--- Formula/th.rb
+++ Formula/th.rb
@@ -1,8 +1,8 @@
 class Th < Formula
   desc ""
   homepage "https://github.com/nikomain/brew-scripts"
-  url "https://github.com/YouLend/homebrew-scripts/archive/refs/tags/th-v1.3.3.tar.gz"
-  sha256 "85f931812874cc79040d5582bf79b23b40e22be567bbafe0fa34752441353a2c"
+  url "https://github.com/YouLend/homebrew-scripts/archive/refs/tags/th-v1.3.4.tar.gz"
+  sha256 "256bb0df081f90ec766e88df35333363540d06ebfc80427e1cb7a8bd47282314"
   license ""
 
   def install
