a59363e9d04a4bd825ff5d9114f1d4c9fd9143f5
nikomain <nikolai.main@youlend.com>
2025-07-28 11:39:43 +0100

    Update th.rb

--- Formula/th.rb
+++ Formula/th.rb
@@ -1,6 +1,6 @@
 class Th < Formula
   desc ""
-  homepage "https://github.com/nikomain/brew-scripts"
+  homepage "https://github.com/YouLend/homebrew-tools"
   url "https://github.com/YouLend/homebrew-scripts/archive/refs/tags/th-v1.4.2.tar.gz"
   sha256 "66050402235c911243341ea672bb78e82db9ee94a87e92bc88e2b27ac1b0ab65"
   license ""
