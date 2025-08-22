3c20748156b9e066d4deb783c69a19231ca00142
nikomain <nikolai.main@youlend.com>
2025-08-22 21:42:15 +0000

    Update th formula to th-v1.5.4

--- Formula/th.rb
+++ Formula/th.rb
@@ -1,8 +1,8 @@
 class Th < Formula
   desc ""
   homepage "https://github.com/YouLend/homebrew-tools"
-  url "https://github.com/YouLend/homebrew-scripts/archive/refs/tags/th-v1.5.3.tar.gz"
-  sha256 "a89ad140126403510260800e05f2eca1130a6e3ee42d025b8875ae563c762531"
+  url "https://github.com/YouLend/homebrew-scripts/archive/refs/tags/th-v1.5.4.tar.gz"
+  sha256 "4e178f4730e97d15c53132f6f5989a095130de98b9d66e899752d7a33e0607a6"
   license ""
 
   def install
